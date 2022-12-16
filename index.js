b9660686f18f5774160b25f04e39b770b2f33a6b
cephalopod <adamhannaford1@gmail.com>
2022-12-16 14:53:34 +0000

    created pseudocode

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+//count the months in the dataset
+    //store data in array
+    //calculate length of array
+    //console.log result
+
+
+
+//calculate total Profit/losses over the entire period
+
+
+
+
+//calculate average of the changes in Profit/losses over entire period
+    //create array of the integral (rate of change)
+    //calculate mean average of this new array
+    //console.log result
+
+
+//find greatest increase in profits over entire period
+    //search intergral list for highest value
+    //console.log result
+
+
+//find the greatest decrease in profits over the entire period
+    //search intergral list for lowest value (largest negative number)
+    //console.log result
+
+
+
+
+
+
